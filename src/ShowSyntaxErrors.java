b26ed32b2f3b0ae49de389e9059b6ef5ccd028d7
764448863@qq.com <764448863@qq.com>
2017-06-20 22:53:25 +0800

    20170620
    Signed-off-by: 764448863@qq.com <764448863@qq.com>

--- src/ShowSyntaxErrors.java
+++ src/ShowSyntaxErrors.java
@@ -2,7 +2,8 @@
  * Created by Administrator on 2017/6/20.
  */
 public class ShowSyntaxErrors {
-    public static void main(String[] agrs){
+    public static void main(String[] agrs) {
+
         System.out.println("welcome to java");
     }
 }
--- src/Welcome.java
+++ src/Welcome.java
@@ -2,7 +2,7 @@
  * Created by Administrator on 2017/6/20.
  */
 public class Welcome {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("welcome to java!");
     }
 }
